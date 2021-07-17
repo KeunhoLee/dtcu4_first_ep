3f866cc344f2e6f29d7bec3d8673f707bb078986
KeunhoLee <hjku1245@gmail.com>
2021-07-17 22:06:30 +0900

    불필요 파일 삭제

--- old/save_text.R
+++
@@ -1,8 +0,0 @@
-twitter_text <- readRDS(get_latest_data("twitter_")) %>%
-  get_text_twitter()
-
-twitter_text <- preprocess_text(twitter_text)
-
-twitter_text %>%
-  unlist() %>%
-  write.table("twitter_text.txt", row.names = FALSE)
